71f7605d2ac2a27b8147269108ba4cb8a9616b7d
Lychuss <raphaelsanjuan6@gmail.com>
2026-08-11 16:31:04 +0800

    created a ui for my dashboard page

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -1,19 +1,42 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 
 export default function Dashboard(){
 
     
 
     return <main className="bg-[url('/backgrounds/clean-quest-background-dashboard.png')] bg-no-repeat bg-cover bg-center min-h-screen w-full">
-        <div className="flex p-4">
-            <div className="text-center text-black font-bold border-yellow-900 rounded-full w-[40px] aspect-square border p-2 bg-gradient-to-r 
+        <div className="flex flex-row p-4 items-center">
+            <div className="relative z-10 text-center text-black font-bold border-yellow-900 rounded-full w-[25px] h-auto border bg-gradient-to-r 
                 from-yellow-900 to-orange-300 z-10">
                 1
             </div>
-            <div className="relative items-center justify-center text-center w-[70px] h-[30px] border rounded-full">
-                9%
+            <div className="relative -ml-4 left-0 z-0 text-center w-[125px] h-[20px] border-yellow-200 border-3 rounded-full overflow-hidden">
+                <div className="absolute h-full inset-y-0 bg-gradient-to-r rounded-full from-yellow-700 to-orange-200 transition-all
+                    duration-700 ease-out"
+                    style={{ width: '50%' }}>
+                </div>
+                <div className="relative font-bold text-sm text-center ">
+                  230/500  
+                </div>
+            </div>
+
+            <Image src="/icons/growth-power.png" alt="growth-power" width={1000} height={1000}
+                className="relative z-10 w-[35px] h-auto" />
+            <div className="relative -ml-4 left-0 z-0 text-center w-[60px] h-[13px] bg-black rounded-full overflow-hidden">
+                <div className="relative font-bold text-xs text-center ">
+                  12,000
+                </div>
+            </div>
+
+            <Image src="/icons/completed-task-v2.png" alt="completed-task" width={1000} height={1000}
+                className="relative z-10 w-[35px] h-auto" />
+            <div className="relative -ml-4 left-0 z-0 text-center w-[60px] h-[13px] bg-black rounded-full overflow-hidden">
+                <div className="relative font-bold text-xs text-center ">
+                  12,000
+                </div>
             </div>
         </div>
     </main>
