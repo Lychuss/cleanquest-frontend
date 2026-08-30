81e06379064e1e08929bbfaa98d6fbabe3b75be6
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-08-31 00:30:21 +0800

    fix all the ui code into a component for clean code

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -9,65 +9,42 @@ import getDashboard from "@/src/services/data/dashboard.api";
 import { Data } from "@/src/model/types/data.type";
 
 import { authClient } from "@/src/libs/authclient";
+import StatsBadge from "@/src/components/ui/statsbadge";
+import PlayerLevel from "@/src/components/ui/playerlevel";
 
 export default function Dashboard(){
 
     const [data, setData] = useState<Data>();
-
     const user = authClient.useSession();
-
     const userId = user.data?.session.id;
 
     useEffect(() => {
 
         if(!userId) return;
 
-        const fetchData = async () => {
-            const response = await getDashboard(userId);
-
-            setData(response)
-        }
-
-        fetchData();
+        getDashboard(userId).then(setData);
+        
     }, [userId])
 
     return <main className="bg-[url('/backgrounds/clean-quest-background-dashboard-v2.png')] bg-no-repeat bg-cover bg-center min-h-screen w-full
-        has-[.living-room:hover]:bg-[url('/backgrounds/dashboard-background-living-room.png')] 
+        has-[.living-room:hover]:bg-[url('/backgrounds/dashboard-background-living-room-v2.png')] 
         has-[.bedroom:hover]:bg-[url('/backgrounds/dashboard-background-room.png')]
         has-[.kitchen:hover]:bg-[url('/backgrounds/dashboard-background-kitchen.png')]">
         <section>
-            <div className="flex flex-row p-4 items-center">
-                <div className="flex p-1 items-center border-transparent rounded-lg  bg-[rgba(20,16,18,0.8)]/30">
-                    <Image src="/icons/level-v1.png" alt="level-icon" width={1000} height={1000} className="w-[50px] h-auto"/> 
-                    <div>
-                        <h1 className="text-[9px] font-semibold">{data?.data.ingameName}</h1>
-                        <h1 className="text-[7px] font-semibold text-[rgba(210,157,6,0.8)] mb-1">Level <span className="text-white">{data?.data.level}</span></h1>
-                        <div className="relative z-0 w-[70px] h-[5px] border rounded-full overflow-hidden">
-                            <div className="absolute inset-y-0 h-full bg-gradient-to-r rounded-full from-yellow-700 to-orange-200
-                                transition-all duration-700 ease-out" style={{ width: `${data?.data.experience}%`}}></div>
-                        </div>
-                        <h1 className="mt-1 text-[7px] text-[rgba(154,154,154,0.8)]">{data?.data.experience}/100</h1>
-                    </div>
-                </div>
+            <div className="flex flex-row p-4 items-center gap-2">
 
-                <Image src="/icons/growth-power.png" alt="growth-power" width={1000} height={1000}
-                    className="relative left-1 z-10 w-[20px] h-auto" />
-                <div className="flex justify-center items-center p-2 -ml-4 left-0 z-0 text-[8px] w-[60px] h-[13px] bg-[rgba(20,16,18,0.8)]/70 rounded-full overflow-hidden">
-                        {data?.data.growth}
-                </div>
+                <PlayerLevel ign={data?.data.ingameName} level={data?.data.level} exp={data?.data.experience} />
 
-                <Image src="/icons/completed-task-v2.png" alt="growth-power" width={1000} height={1000}
-                    className="relative left-1 z-10 w-[20px] h-auto" />
-                <div className="flex justify-center items-center p-2 -ml-4 left-0 z-0 text-[8px] w-[60px] h-[13px] bg-[rgba(20,16,18,0.8)]/70 rounded-full overflow-hidden">
-                        {data?.data.completedTask}
-                </div>
+                <StatsBadge icon="/icons/growth-power.png" alt="growth-power" value={data?.data.growth} />
+                <StatsBadge icon="/icons/completed-task-v2.png" alt="completed-task" value={data?.data.completedTask} />
+
             </div>
         </section>
 
         <section>
-            <h1 className="living-room cursor-pointer">Living Room</h1>
-            <h1 className="bedroom cursor-pointer">Bedroom</h1>
-            <h1 className="kitchen cursor-pointer">Kitchen</h1>
+            <div className="absolute z-0 left-20 top-90 living-room cursor-pointer w-[225px] h-[200px]"></div>
+            <div className="absolute z-0 left-20 bottom-70 bedroom cursor-pointer w-[100px] h-[230px]"></div>
+            <div className="absolute z-0 right-0 bottom-80 kitchen cursor-pointer w-[100px] h-[180px]"></div>
         </section>
     </main>
 }
