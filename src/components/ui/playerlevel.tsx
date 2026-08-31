ec18aad7f6f2a62e0de48c52ae674856dc1fd692
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-08-31 14:23:18 +0800

    fix the level ui

--- src/components/ui/playerlevel.tsx
+++ src/components/ui/playerlevel.tsx
@@ -7,11 +7,11 @@ type MyProps = {
 }
 
 export default function PlayerLevel(props: MyProps){
-    return <div className="flex p-1 items-center border-transparent rounded-lg  bg-[rgba(20,16,18,0.8)]/30">
-                <Image src="/icons/level-v1.png" alt="level-icon" width={1000} height={1000} className="w-[50px] h-auto"/> 
-                <div>
-                    <h1 className="text-[9px] font-semibold text-white">{props.ign}</h1>
-                    <h1 className="text-[7px] font-semibold text-[rgba(210,157,6,0.8)] mb-1">Level <span className="text-white">{props.level}</span></h1>
+    return <div className="flex p-1 items-center bg-[url('/icons/level-v2.png')] bg-cover bg-center w-[133px] h-[60px]">
+                <Image src="/icons/level-v1.png" alt="level-icon" width={1000} height={1000} className="w-[36px] h-auto"/> 
+                <div className="p-2">
+                    <h1 className="text-[8px] font-semibold mb-1">{props.ign}</h1>
+                    <h1 className="text-[6px] font-semibold text-[rgba(210,157,6,0.8)]">Level <span className="text-white">{props.level}</span></h1>
                     <div className="relative z-0 w-[70px] h-[5px] border rounded-full overflow-hidden">
                         <div className="absolute inset-y-0 h-full bg-gradient-to-r rounded-full from-yellow-700 to-orange-200
                             transition-all duration-700 ease-out" style={{ width: `${props.exp}%`}}></div>
