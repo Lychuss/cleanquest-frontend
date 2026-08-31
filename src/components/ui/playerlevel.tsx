4e28c19c98e42c930e63fac47d6af6c18935c495
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-08-31 10:36:31 +0800

    change the name and others into text white

--- src/components/ui/playerlevel.tsx
+++ src/components/ui/playerlevel.tsx
@@ -10,7 +10,7 @@ export default function PlayerLevel(props: MyProps){
     return <div className="flex p-1 items-center border-transparent rounded-lg  bg-[rgba(20,16,18,0.8)]/30">
                 <Image src="/icons/level-v1.png" alt="level-icon" width={1000} height={1000} className="w-[50px] h-auto"/> 
                 <div>
-                    <h1 className="text-[9px] font-semibold">{props.ign}</h1>
+                    <h1 className="text-[9px] font-semibold text-white">{props.ign}</h1>
                     <h1 className="text-[7px] font-semibold text-[rgba(210,157,6,0.8)] mb-1">Level <span className="text-white">{props.level}</span></h1>
                     <div className="relative z-0 w-[70px] h-[5px] border rounded-full overflow-hidden">
                         <div className="absolute inset-y-0 h-full bg-gradient-to-r rounded-full from-yellow-700 to-orange-200
--- src/components/ui/statsbadge.tsx
+++ src/components/ui/statsbadge.tsx
@@ -10,7 +10,8 @@ export default function StatsBadge(props: MyProps){
     return <>
         <Image src={props.icon} alt={props.alt} width={1000} height={1000}
             className="relative left-3 z-10 w-[20px] h-auto" />
-        <div className="flex justify-center items-center p-2 -ml-4 left-0 z-0 text-[8px] w-[60px] h-[13px] bg-[rgba(20,16,18,0.8)]/70 rounded-full overflow-hidden">
+        <div className="flex justify-center items-center p-2 -ml-4 left-0 z-0 text-[8px] w-[60px] h-[13px] bg-[rgba(20,16,18,0.8)]/70 
+            rounded-full overflow-hidde text-white">
                 {props.value}
         </div>
     </>
